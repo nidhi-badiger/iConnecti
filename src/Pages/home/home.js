cb6d3748e61623b32d97a9dbde1fd504e49d758f
nidhi-badiger <nidhibadiger253@gmail.com>
2023-06-05 12:40:49 +0530

    homge page

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import Navbar from "./src/Components/Navbar";
 import Login from '../login/login';
+import Topbar from '../../Components/Topbar';
+import TextSlider from '../../Components/TextSlider/TextSlider';
 
 function Home() {
 	return (
 		<React.Fragment>
-			{/* <Navbar/> */}
+			<Topbar/>
+			<TextSlider/>
             <Login/>
 		</React.Fragment>
 	);
--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -1,25 +1,83 @@
 import React from 'react'
-// import "./login.css"
+import { useState } from "react";
+import "./login.css"
+import FormInput from './FormInput';
+
 const Login = () => {
+
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+
   return (
     <div className='login'>
     <div className='loginWrapper'>
-             
-        <div className='loginRight'>
 
             <div className='loginStart'>
-            <input placeholder='username' className='password' />
-            <input placeholder='password' className='password' />
-       
-            <button className='SignInButton' >Sign In</button>
-            <span className='passwordForgot'>Forgot Password?</span>
-            <button className='loginRegisterButton'>Create a account</button>
+            <form onSubmit={handleSubmit} >
+            
+            {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+            <div className='loginEnd'>
+            <button className='SignInButton' >Login</button>
+            <p className='passwordForgot'>Forgot Password?</p>
             </div>
+            
+            </form>
+            </div>
+            
+            <p>Not yet Registered? Register as</p>
+            <div className='registerButtons'>
+            <button className='RegisterButton'>Register as Company SPOC</button>
+            <button className='RegisterButton'>Register as College SPOC</button>
+            <button className='RegisterButton'>Register as Student</button>
+            </div>
+
         </div>
     </div>
 
-    </div>
-  )
+  );
 }
 
 export default Login
