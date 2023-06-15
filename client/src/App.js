7a9683d836ef8aac6c7cd345576336a4cb9988f6
Sarvesh Chandnani <92716350+SarveshChandnani@users.noreply.github.com>
2023-06-16 01:39:07 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
-import Home from './Pages/home/home';
-import Activation from './Pages/Activation/Activation';
-import Register from './Pages/register/register'; 
+import Home from './pages/home/home';
+import  {Routes ,Route} from 'react-router-dom';
+import Register from "./pages/register/register";
+import Activation from './pages/Activation/Activation';
+import MainScreen from './pages/mainScreen/MainScreen';
+import AdminLogin from './pages/Admin/AdminLogin';
+import AdminPage from './pages/Admin/AdminPage';
 
 
 function App() {
 	return (
 		<div>
+		
 		<Routes>
-			<Route path='/' element={<Home/>}> </Route>
-			<Route path='Activation' element={<Activation/>}> </Route>
-			<Route path='Register' element={<Register/>}> </Route>
 
-		   
-		   </Routes>
-		   
+			<Route path='/' element={<Home/>}> </Route>
+			<Route path='MainScreen/Activation' element={<Activation/>}> </Route>
+			<Route path='/Register' element={<Register/>}> </Route>
+			<Route path='/MainScreen' element={<MainScreen/>}> </Route>
+            <Route path='/AdminLogin' element={<AdminLogin/>}> </Route>
+			<Route path='/AdminLogin/AdminPage' element={<AdminPage/>}> </Route>
+		</Routes >
+		
 		</div>
 	);
 }
